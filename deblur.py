1bfbd15254156da84fd4cf782cb2dd73506f2cee
C N Tejas <64399192+cntejas@users.noreply.github.com>
2025-01-27 23:38:21 +0900

    Update deblur.py

--- deblur.py
+++ deblur.py
@@ -1,8 +1,8 @@
 import os
-combine="python /content/DeblurGAN/datasets/combine_A_and_B.py --fold_A /content/DeblurGAN/datasets/datasets/blurred --fold_B /content/DeblurGAN/datasets/datasets/clear --fold_AB /content/DeblurGAN/datasets/datasets/blur2clear"
+combine="python /content/DeblurGAN/datasets/combine_A_and_B.py --fold_A /content/DeblurGAN/datasets/blurred --fold_B /content/DeblurGAN/datasets/clear --fold_AB /content/DeblurGAN/datasets/blur2clear"
 os.system(combine)
-train = "python /content/DeblurGAN/train.py --dataroot /content/DeblurGAN/datasets/datasets/blur2clear --learn_residual --resize_or_crop 'scale_width'"
+train = "python /content/DeblurGAN/train.py --dataroot /content/DeblurGAN/datasets/blur2clear --learn_residual --resize_or_crop 'scale_width'"
 os.system(train)
-test = "python /content/DeblurGAN/test.py --dataroot /content/DeblurGAN/datasets/datasets/test --model test --dataset_mode single --learn_residual --resize_or_crop 'scale_width'"
+test = "python /content/DeblurGAN/test.py --dataroot /content/DeblurGAN/datasets/test --model test --dataset_mode single --learn_residual --resize_or_crop 'scale_width'"
 # Execute the command
 os.system(test)
